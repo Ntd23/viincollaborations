3d19c093de1a2b69f3cfdb04d732c7b488357e7b
HoangQuan <104688578+chapilcheapz@users.noreply.github.com>
2026-05-26 11:46:56 +0700

    chore: remove SePay payment plugin from platform

--- platform/plugins/fob-sepay/helpers/constants.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (! defined('SEPAY_PAYMENT_METHOD_NAME')) {
-    define('SEPAY_PAYMENT_METHOD_NAME', 'sepay');
-}
-
-if (! defined('SEPAY_FOB_URL')) {
-    define('SEPAY_FOB_URL', 'https://friendsofbotble.com');
-}
--- platform/plugins/fob-sepay/resources/views/payment-method.blade.php
+++
@@ -1,6 +0,0 @@
-@if (setting('payment_sepay_status') == 1 && setting('payment_sepay_bank_account_number') && setting('payment_sepay_bank_short_name'))
-    <x-plugins-payment::payment-method
-        :name="SEPAY_PAYMENT_METHOD_NAME"
-        paymentName="SePay"
-    />
-@endif
